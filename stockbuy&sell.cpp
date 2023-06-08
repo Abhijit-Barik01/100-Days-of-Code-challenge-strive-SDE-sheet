03a4b3165a6e4ae077c658e66f8249c1a72256db
Abhijit Barik <71961635+Abhijit-Barik01@users.noreply.github.com>
2023-06-08 22:55:24 +0530

    Create stockbuy&sell.cpp

---
+++ stockbuy&sell.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    // Write your code here.
+
+
+    int mini =INT_MAX;
+    int profit =0;
+    for(int i=0; i<prices.size();i++)
+    {
+         mini=min(prices[i],mini);
+        profit=max(profit,prices[i]-mini);
+
+       
+    }
+
+    return  profit;
+}

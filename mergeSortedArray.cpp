2f95c506b15f98b5628dccd6999639e1e476c03c
Abhijit Barik <71961635+Abhijit-Barik01@users.noreply.github.com>
2023-06-10 21:40:05 +0530

    Update mergeSortedArray.cpp

--- mergeSortedArray.cpp
+++ mergeSortedArray.cpp
@@ -1,1 +1,41 @@
-
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=0;
+        int j=0;
+        int k=0;
+         vector<int>c(m+n);
+        while(i <m && j< n)
+        {
+            if(nums1[i] <= nums2[j])
+            {
+                c[k]=nums1[i];
+                k++;
+                i++;
+            }
+            else{
+                c[k]=nums2[j];
+                j++;
+                k++;
+            }
+        }
+        
+        while(i <m)
+        {
+           c[k++]=nums1[i];
+                i++; 
+        }
+        
+         
+        while(j <n)
+        {
+           c[k++]=nums2[j];
+                j++; 
+        }
+        for(int i=0;i<n+m;i++)
+        {
+            nums1[i]=c[i];
+        }
+        
+    }
+};
